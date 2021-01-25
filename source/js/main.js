3fbccf48123577d1bb8657c0b5f9f59c61e3b024
Sergey Vasilyev <kosh.goo@gmail.com>
2021-01-25 20:38:14 +0300

    Подготовка проекта

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+import {ieFix} from './utils/ie-fix';
+import {iosVhFix} from './utils/ios-vh-fix';
+
+// Utils
+// ---------------------------------
+
+ieFix();
+iosVhFix();
+
+// Modules
+// ---------------------------------
